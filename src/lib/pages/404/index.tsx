22fd80aa1c6da1129ecf0d0769d619e051f7ab07
federico-it <fedecor@protonmail.com>
2022-11-08 00:44:26 +0100

    feat: page 404

--- src/lib/pages/404/index.tsx
+++ src/lib/pages/404/index.tsx
@@ -4,7 +4,6 @@ import {
   Heading,
   Image,
   Text,
-  Link as ChakraLink,
   useColorMode,
   Flex,
 } from "@chakra-ui/react";
@@ -22,23 +21,11 @@ const Page404 = () => {
       <MotionBox
         animate={{ y: 20 }}
         transition={{ repeat: Infinity, duration: 2, repeatType: "reverse" }}
-        width={{ base: "100%", sm: "70%", md: "60%" }}
+        width={{ base: "50%", sm: "50%", md: "35%" }}
         margin="0 auto"
       >
-        <Image
-          src="/404 Error-pana.svg"
-          alt="Error 404 not found Illustration"
-        />
+        <Image src="/no404.png" alt="Error 404 not found Illustration" />
       </MotionBox>
-      <Text textAlign="center" fontSize="xs" color="gray">
-        <ChakraLink
-          href="https://stories.freepik.com/web"
-          isExternal
-          rel="noopener noreferrer"
-        >
-          Illustration by Freepik Stories
-        </ChakraLink>
-      </Text>
 
       <Box marginY={4}>
         <Heading textAlign="center" size="lg">
@@ -52,7 +39,7 @@ const Page404 = () => {
           <Button
             as={Link}
             href="/"
-            backgroundColor={colorMode === "light" ? "gray.300" : "teal.500"}
+            backgroundColor={colorMode === "light" ? "gray.300" : "gray.500"}
             size="sm"
           >
             Let&apos;s Head Back
