48a39707725a1a71757c38955d9a41b5b0523b07
federico-it <mail@fede.me>
2024-03-08 22:00:10 +0100

    edit

--- next.config.js
+++ next.config.js
@@ -1,36 +1,56 @@
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable:
-    process.env.NODE_ENV === "development" 
-  // delete two lines above to enable PWA in production deployment
-  // add your own icons to public/manifest.json
-  // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+  disable: process.env.NODE_ENV === "development",
 });
-/** @type {import('next').NextConfig} */
-module.exports = {
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = withPWA({
   webpack: (config, { isServer }) => {
-  config.module.rules.push({
-    test: /\.(mp3)$/,
-    use: {
-      loader: 'file-loader',
-      options: {
-        publicPath: '/_next/static/sounds/',
-        outputPath: 'static/sounds/',
-        name: '[name].[ext]',
-        esModule: false
-      }
+    // Code splitting
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    };
+
+    // Minify and optimize the bundle
+    if (!isServer) {
+      config.optimization.minimize = true;
+
+      // Initialize minimizer array if undefined
+      config.optimization.minimizer = config.optimization.minimizer || [];
+
+      // Set terserOptions
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+        })
+      );
     }
-  })
 
-  
+    // File loader for MP3 files
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/sounds/",
+          outputPath: "static/sounds/",
+          name: "[name].[ext]",
+          esModule: false,
+        },
+      },
+    });
 
-  return config
-}}
-
-withPWA({
-  swcMinify: true,
-  eslint: {
-    dirs: ["src"],
+    return config;
   },
-  compress: true,
-})
+});
