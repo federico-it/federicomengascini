7527ca8bc67780d1d920005c7720a9e9de46f8b3
federico-it <mail@fede.me>
2024-03-08 15:15:50 +0100

    edit

--- src/lib/components/samples/SomeText.tsx
+++ src/lib/components/samples/SomeText.tsx
@@ -1,19 +1,34 @@
 import { Box, Grid, Heading, Text } from "@chakra-ui/react";
+import { keyframes } from "@emotion/react";
 
-
+const waveAnimation = keyframes`
+  0%, 100% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(15deg);
+  }
+  50% {
+    transform: rotate(0deg);
+  }
+  75% {
+    transform: rotate(-15deg);
+  }
+`;
 
 const SomeText = () => {
   return (
-    /* eslint-disable */
     <Grid textAlign="center">
       <Heading as="h1" size="lg">
         I'M FEDERICO{" "}
         <Box
-          
           width={{ base: "100%", sm: "70%", md: "60%" }}
           margin="0 auto"
+          animation={`${waveAnimation} 1s linear infinite`} // Cambiato da "steps" a "linear"
         >
-          <Text>👋🏻</Text>
+          <Text as="span" fontSize="xl" display="inline-block" verticalAlign="middle">
+            👋🏻
+          </Text>
         </Box>
       </Heading>
 
